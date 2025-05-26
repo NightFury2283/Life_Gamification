68dca00cf98c9210c2dfc7149d57b96698a7e7cf
Александр Клинный <klinnyy.a.i@edu.mirea.ru>
2025-05-26 16:07:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/life_gamification/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.life_gamification
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.life_gamification.ui.theme.Life_GamificationTheme
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val fs = Firebase.firestore
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Life_GamificationTheme {
+        Greeting("Android")
+    }
+}
